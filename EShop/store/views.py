1d3b56f2779048bc0b1a0e667e12282f353e5ad8
unknown <thapaachyut8863>
2021-03-19 21:41:41 +0545

    first commit

---
+++ EShop/store/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+# Create your views here.
+def index(request):
+    products = Product.get_all_products();
+    return render(request, 'index.html', {'products': products})
